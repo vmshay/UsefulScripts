39509d0ed76f2ec4d188794bead39897a0406e73
Vildan Shaymagsumov <37113778+vmshay@users.noreply.github.com>
2022-02-18 00:44:00 +0700

    Simple Prntsc scraper

---
+++ Prntsc_Scraper.ps1
@@ -0,0 +1,10 @@
+$Fail=0;
+$count = 0;
+While($true)
+{
+[string]$URI = (48..57) + (97..122) | Get-random -Count 5 | foreach{[char[]]$_}
+$URI = $URI.Replace(" ","")
+Try {wget -Uri (curl https://prnt.sc/$URI).Images[0].src -OutFile "F:\prtscr\$URI.jpg"}
+Catch { $Fail += 1;}
+$URI
+}
