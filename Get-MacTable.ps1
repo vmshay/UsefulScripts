e8bbd72de6654624d7fe442e4d1fee9d396d0ee5
Vildan Shaymagsumov <37113778+vmshay@users.noreply.github.com>
2022-02-18 23:45:15 +0700

    Get mac address table from network devices

---
+++ Get-MacTable.ps1
@@ -0,0 +1,70 @@
+﻿function Get-MacTable{
+    
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Login,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Password,
+
+        [Parameter(Mandatory=$true)]
+        [string]$IP,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Port,
+
+        [Parameter(Mandatory=$true)]
+        [string]$DestinationFolder,
+
+        [Parameter(Mandatory=$true)]
+        [string]$FileName,
+
+        [Parameter(Mandatory=$false)]
+        [bool]$remove=$false,
+
+        [Parameter(Mandatory=$false)]
+        [bool]$SingleFile
+    )
+
+#Vars
+$result = @()
+
+
+#Build credential
+$Credential = New-Object System.Management.Automation.PSCredential ($Login,(ConvertTo-SecureString $Password -AsPlainText -Force))
+
+
+#SSH#
+$Session = New-SSHSession -Credential $Credential -ComputerName $IP -Port $Port -AcceptKey:$true
+$response = (Invoke-SSHCommand -SSHSession $Session -Command "sh mac-address-table").Output
+Remove-SSHSession -SSHSession $Session
+
+
+
+$device = $response[0] -replace ">sh mac-address-table"
+
+for($i = 4; $i -lt $response.Length-1;$i++){
+    $vlan = ($response[$i] -split " " | ? {$_})[0]
+    $MAC = ($response[$i] -split " " | ? {$_})[1]
+    $Interface = ($response[$i] -split " " | ? {$_})[4]
+    $obj = Select-Object @{n='Device';e={"$device"}},@{n='VLAN';e={"$vlan"}},@{n='MAC-Address';e={"$MAC"}},@{n='Interface';e={"$Interface"}} -InputObject ''
+    $result += $obj
+}
+
+
+if((Test-Path $DestinationFolder\$FileName) -and !$SingleFile){
+    rm -Path $DestinationFolder\$FileName
+    $result | Export-Excel -Path $DestinationFolder\$FileName -AutoSize
+    }
+elseif($SingleFile){
+    $result | Export-Excel -Path $DestinationFolder\$FileName -AutoSize -Append
+    }
+else{
+    $result | Export-Excel -Path $DestinationFolder\$FileName -AutoSize
+    }
+}
+
+
+
+
